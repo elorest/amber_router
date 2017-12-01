402caedaf21132db49f8a74cb178114a92029a09
Isaac Sloan <isaac@isaacsloan.com>
2017-12-01 00:43:09 -0700

    benchmarks

--- src/benchmark.cr
+++ src/benchmark.cr
@@ -2,30 +2,95 @@ require "radix"
 require "./route_set"
 require "benchmark"
 
+ROUTE_LIBRARY = {
+  "/get/"                                                                               => :root,
+  "/get/users/:id"                                                                      => :users,
+  "/get/users/:id/books"                                                                => :users_books,
+  "/get/books/:id"                                                                      => :books,
+  "/get/books/:id/chapters"                                                             => :book_chapters,
+  "/get/books/:id/authors"                                                              => :book_authors,
+  "/get/books/:id/pictures"                                                             => :book_pictures,
+  "/get/a/b/c/d/e/f/g/h/i/j/k/l/m/n/o/p/q/r/s/t/u/v/w/x/y/z"                            => :alphabet,
+  "/get/var/:b/:c/:d/:e/:f/:g/:h/:i/:j/:k/:l/:m/:n/:o/:p/:q/:r/:s/:t/:u/:v/:w/:x/:y/:z" => :variable_alphabet,
+  "/get/foobarbizfoobarbizfoobarbizfoobarbizfoobarbizbat/:id"                           => :foobar_bat,
+  "/get/foobarbizfoobarbizfoobarbizfoobarbizfoobarbizbom/:id"                           => :foobar_bom,
+}
 
+module Jewel
+  class Result
+    # property params : Hash(String, String)
+    property payload : Symbol? = nil
+    setter params : Regex::MatchData?
+
+    def initialize(@payload, @params)
+    end
+
+
+    def found?
+      !@payload.nil?
+    end
+
+    def params
+      if @params
+        Hash(String, String).new.tap do |h|
+          params.to_h.each { |k, v| h[k.to_s] = v.to_s unless k.is_a?(Int32) }
+        end
+      else
+        Hash(String, String).new
+      end
+    end
+  end
+
+  ROUTES = {} of Nil => Nil
+
+  macro routes(path, body)
+    {% ROUTES[path.gsub(/\*/, "~")] = body %}
+  end
+
+  macro finished
+    class Routes
+      def find(path)
+        case path
+          {% for path in ROUTES.keys %}
+            {% if path.includes?(':') %}
+            when /^{{path.split("/").map { |p| p.starts_with?(':') ? "(?<" + p.gsub(/\:/, "") + ">[^$\\/]+)" : p }.join("\\/").id}}$/
+              return Result.new({{ROUTES[path]}}, $~)
+            {% else %}
+              {% if path.includes?('~') %}
+              when /^{{(path.gsub(/\//, "\\/").split("~").first + "[^$]").id}}$/
+                return Result.new({{ROUTES[path]}}, nil)
+              {% else %}
+              when {{path}}
+                return Result.new({{ROUTES[path]}}, nil)
+              {% end %}
+            {% end %}
+          {% end %}
+        else
+          Result.new(nil, nil)
+        end
+      end
+    end
+  end
+end
+
+{% for path, body in ROUTE_LIBRARY %}
+  Jewel.routes({{path}}, {{body}})
+{% end %}
+
+# router = Jewel::Routes.new
+# puts router.find("/get/books/23/chapters").inspect
 class Benchmarker
   getter route_library
   getter route_checks
   getter amber_router
   getter radix_router
+  getter jewel_router
 
   def initialize
-    @route_library = {
-      "/get/"                   => :root,
-      "/get/users/:id"          => :users,
-      "/get/users/:id/books"    => :users_books,
-      "/get/books/:id"          => :books,
-      "/get/books/:id/chapters" => :book_chapters,
-      "/get/books/:id/authors"  => :book_authors,
-      "/get/books/:id/pictures" => :book_pictures,
-      "/get/a/b/c/d/e/f/g/h/i/j/k/l/m/n/o/p/q/r/s/t/u/v/w/x/y/z" => :alphabet,
-      "/get/var/:b/:c/:d/:e/:f/:g/:h/:i/:j/:k/:l/:m/:n/:o/:p/:q/:r/:s/:t/:u/:v/:w/:x/:y/:z" => :variable_alphabet,
-      "/get/foobarbizfoobarbizfoobarbizfoobarbizfoobarbizbat/:id"   => :foobar_bat,
-      "/get/foobarbizfoobarbizfoobarbizfoobarbizfoobarbizbom/:id"   => :foobar_bom
-    }
-
+    @route_library = ROUTE_LIBRARY
     @amber_router = Amber::Router::RouteSet(Symbol).new
     @radix_router = Radix::Tree(Symbol).new
+    @jewel_router = Jewel::Routes.new
 
     route_library.each do |k, v|
       radix_router.add(k, v)
@@ -51,10 +116,11 @@ class Benchmarker
   def compare(name : String, route : String, result : Symbol?)
     puts route
 
-     Benchmark.ips do |x|
-       x.report("router: #{name}") { run_check(amber_router, route, result) }
-       x.report("radix: #{name}") { run_check(radix_router, route, result) }
-     end
+    Benchmark.ips do |x|
+      x.report("jewel: #{name}") { run_check(jewel_router, route, result) }
+      x.report("router: #{name}") { run_check(amber_router, route, result) }
+      x.report("radix: #{name}") { run_check(radix_router, route, result) }
+    end
 
     puts
     puts
